d570fac959e566526aa6400c9abc56ad508d75cc
Laurent Curau <laurentcurau@gmail.com>
2014-11-21 18:08:45 +0000

    Board marks a space with mark 'X' or 'O'

--- lib/board.rb
+++ lib/board.rb
@@ -6,5 +6,9 @@ module TicTacToe
     def initialize(grid=[1,2,3,4,5,6,7,8,9])
       @grid = grid
     end
+
+    def mark_space(space, mark)
+      @grid[space - 1] = mark
+    end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,5 +6,13 @@ describe TicTacToe::Board do
   it "has a 3x3 grid" do
     expect(board.grid).to eql [1,2,3,4,5,6,7,8,9]
   end
+
+  describe "#mark_space" do
+    it "marks a space with a mark" do
+      mark = ["X", "O"].sample
+      board.mark_space(1, mark)
+      expect(board.grid).to eql [mark, 2,3,4,5,6,7,8,9]
+    end
+  end
 end
 
