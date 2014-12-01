93e6e028e8f35ef8211f120a71252c88ed6c5722
Laurent Curau <laurentcurau@gmail.com>
2014-12-01 15:40:41 +0000

    Refactor tests: extract into #let rspec helper

--- spec/tic_tac_toe/display_spec.rb
+++ spec/tic_tac_toe/display_spec.rb
@@ -1,11 +1,13 @@
 require 'tic_tac_toe/display'
 
 describe TicTacToe::Display do
+
+  let(:input) { StringIO.new }
+  let(:output) { StringIO.new }
+  let(:display) { TicTacToe::Display.new(input: input, output: output) }
+
   describe "#game_title" do
     it "shows game title" do
-      input = StringIO.new
-      output = StringIO.new
-      display = TicTacToe::Display.new(input: input, output: output)
       display.game_title
       expect(output.string).to eql "** TicTacToe **\n"
     end
@@ -13,9 +15,6 @@ describe TicTacToe::Display do
 
   describe "#choice_of_mark" do
     it "asks human player to choose a mark X or O" do
-      input = StringIO.new
-      output = StringIO.new
-      display = TicTacToe::Display.new(input: input, output: output)
       display.choice_of_mark
       expect(output.string).to eql "Choose your mark:\n1: X\n 2: O\n"
     end
--- spec/tic_tac_toe/game_controller_spec.rb
+++ spec/tic_tac_toe/game_controller_spec.rb
@@ -3,12 +3,13 @@ require 'tic_tac_toe/display'
 
 describe TicTacToe::GameController do
 
+  let(:input) { StringIO.new }
+  let(:output) { StringIO.new }
+  let(:display) { TicTacToe::Display.new(input: input, output: output) }
+  let(:game) { TicTacToe::GameController.new(display: display) }
+
   describe "#display_game_title" do
     it "displays game title" do
-      input = StringIO.new
-      output = StringIO.new
-      display = TicTacToe::Display.new(input: input, output: output)
-      game = TicTacToe::GameController.new(display: display)
       game.display_game_title
       expect(output.string).to eql "** TicTacToe **\n"
     end
@@ -16,10 +17,6 @@ describe TicTacToe::GameController do
 
   describe "#display_choice_of_mark" do
     it "tells the human player to choose X or O mark" do
-      input = StringIO.new
-      output = StringIO.new
-      display = TicTacToe::Display.new(input: input, output: output)
-      game = TicTacToe::GameController.new(display: display)
       game.display_choice_of_mark
       expect(output.string).to eql "Choose your mark:\n1: X\n 2: O\n"
     end
