6342364a5e8fea2e8a2ea1202d8088c42ef1a9ca
Laurent Curau <laurentcurau@gmail.com>
2014-12-01 15:43:23 +0000

    Rename game to controller

--- spec/tic_tac_toe/game_controller_spec.rb
+++ spec/tic_tac_toe/game_controller_spec.rb
@@ -6,18 +6,18 @@ describe TicTacToe::GameController do
   let(:input) { StringIO.new }
   let(:output) { StringIO.new }
   let(:display) { TicTacToe::Display.new(input: input, output: output) }
-  let(:game) { TicTacToe::GameController.new(display: display) }
+  let(:controller) { TicTacToe::GameController.new(display: display) }
 
   describe "#display_game_title" do
     it "displays game title" do
-      game.display_game_title
+      controller.display_game_title
       expect(output.string).to eql "** TicTacToe **\n"
     end
   end
 
   describe "#display_choice_of_mark" do
     it "tells the human player to choose X or O mark" do
-      game.display_choice_of_mark
+      controller.display_choice_of_mark
       expect(output.string).to eql "Choose your mark:\n1: X\n 2: O\n"
     end
   end
