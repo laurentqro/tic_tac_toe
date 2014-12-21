5545f5e7eeea892c8b84c8a3c5852573b67c7686
Laurent Curau <laurentcurau@gmail.com>
2014-12-21 01:03:21 +0000

    Remove logic that is not GameController's responsibility

--- lib/tic_tac_toe/game_controller.rb
+++ lib/tic_tac_toe/game_controller.rb
@@ -1,17 +1,14 @@
 module TicTacToe
   class GameController
-    attr_reader :display, :game
+    attr_reader :game
 
-    def initialize(display: display, game: game)
-      @display = display
+    def initialize(game: game)
       @game = game
     end
 
     def start
-      display_choice_of_mark
+      game.set_human_mark
       display_board
-      set_human_mark
-      set_computer_mark
 
       until game.is_over? do
         make_move
@@ -22,20 +19,8 @@ module TicTacToe
       display_game_outcome
     end
 
-    def display_choice_of_mark
-      display.get_mark
-    end
-
     def display_board
       game.display_board
-    end
-
-    def set_computer_mark
-      game.computer.choose_mark(game.board)
-    end
-
-    def set_human_mark
-      game.human.mark = parse(display.get_mark)
     end
 
     def make_move
@@ -48,12 +33,6 @@ module TicTacToe
 
     def display_game_outcome
       game.display_outcome
-    end
-
-    private
-
-    def parse(choice_of_mark)
-      choice_of_mark == 1 ? "X" : "O"
     end
   end
 end
--- spec/tic_tac_toe/game_controller_spec.rb
+++ spec/tic_tac_toe/game_controller_spec.rb
@@ -11,57 +11,13 @@ describe TicTacToe::GameController do
   let(:human) { TicTacToe::Human.new }
   let(:computer) { TicTacToe::Computer.new }
   let(:game) { TicTacToe::Game.new(board: board, computer: computer, human: human) }
-  let(:controller) { TicTacToe::GameController.new(display: display, game: game) }
-
-  describe "#display_choice_of_mark" do
-    it "tells the human player to choose X or O mark" do
-      allow(display).to receive(:get_mark)
-      controller.display_choice_of_mark
-      expect(display).to have_received(:get_mark)
-    end
-  end
+  let(:controller) { TicTacToe::GameController.new(game: game) }
 
   describe "#display_board" do
     it "tells the game to display board" do
       allow(game).to receive(:display_board)
       controller.display_board
       expect(game).to have_received(:display_board)
-    end
-  end
-
-  describe "#set_computer_mark" do
-    it "sets computer mark to O if first player picked X" do
-      board = TicTacToe::Board.new(["X", 2, 3, 4, 5, 6, 7, 8, 9])
-      game = TicTacToe::Game.new(board: board, computer: computer, human: human)
-      controller = TicTacToe::GameController.new(display: display, game: game)
-      controller.set_computer_mark
-      expect(game.computer.mark).to eql "O"
-    end
-
-    it "sets computer mark to X if first player picked O" do
-      board = TicTacToe::Board.new(["O", 2, 3, 4, 5, 6, 7, 8, 9])
-      game = TicTacToe::Game.new(board: board, computer: computer, human: human)
-      controller = TicTacToe::GameController.new(display: display, game: game)
-      controller.set_computer_mark
-      expect(game.computer.mark).to eql "X"
-    end
-  end
-
-  describe "#set_human_mark" do
-    it "sets human mark to X" do
-      input = StringIO.new("1\n")
-      display = TicTacToe::Display.new(input: input, output: output)
-      controller = TicTacToe::GameController.new(display: display, game: game)
-      controller.set_human_mark
-      expect(game.human.mark).to eql "X"
-    end
-
-    it "sets human mark to O" do
-      input = StringIO.new("2\n")
-      display = TicTacToe::Display.new(input: input, output: output)
-      controller = TicTacToe::GameController.new(display: display, game: game)
-      controller.set_human_mark
-      expect(game.human.mark).to eql "O"
     end
   end
 
@@ -73,7 +29,7 @@ describe TicTacToe::GameController do
   end
 
   describe "#next_player" do
-    it "tells the game to switche current player" do
+    it "tells the game to switch current player" do
       allow(game).to receive(:next_player)
       controller.next_player
       expect(game).to have_received(:next_player)
