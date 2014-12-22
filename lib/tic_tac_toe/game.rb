a0b3d4557d3a54eae8173edcf1bcbce6a46c9ac8
Laurent Curau <laurentcurau@gmail.com>
2014-12-22 18:51:12 +0100

    Refactor #play and #make_move

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -8,17 +8,16 @@ module TicTacToe
       @computer = computer
       @human = human
       @display = display
+      @human.display = @display
       @current_player = human
     end
 
     def play
       set_human_mark
-      display_board
+      display.output_board(board.grid)
 
-      until is_over? do
-        move = get_move_from(current_player)
-        make_move(move, current_player)
-        display_board
+      until board.winner? || board.draw?  do
+        make_move(current_player)
         next_player
       end
 
@@ -29,41 +28,27 @@ module TicTacToe
       human.mark = parse(display.get_mark)
     end
 
-    def is_over?
-      is_won? || is_draw?
-    end
-
-    def is_won?
-      board.winner?
-    end
-
-    def is_draw?
-      board.draw?
-    end
-
     def next_player
       @current_player == human ? @current_player = computer : @current_player = human
     end
 
-    def make_move(move, player)
+    def make_move(player)
+      move = player.pick_move(board)
       board.mark_space(move, player.mark)
       display.clear_screen
-    end
-
-    def display_board
       display.output_board(board.grid)
     end
 
     def display_outcome
-      if is_won? && current_player == human
+      if board.winner? && current_player == human
         display.output_win_announcement
       end
 
-      if is_won? && current_player != human
+      if board.winner? && current_player != human
         display.output_loss_announcement
       end
 
-      if is_draw?
+      if board.draw?
         display.output_draw_announcement
       end
     end
@@ -72,14 +57,6 @@ module TicTacToe
 
     def parse(choice_of_mark)
       choice_of_mark == 1 ? "X" : "O"
-    end
-
-    def get_move_from(current_player)
-      if current_player == human
-        display.get_move
-      else
-        computer.pick_move(board)
-      end
     end
 
   end
