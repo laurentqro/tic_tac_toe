ede00030689b8ff0181508ec14ff7bd4fea1d91b
Laurent Curau <laurentcurau@gmail.com>
2015-01-13 16:10:10 +0000

    GameState##generate_successors generates list of successor game states

--- lib/tic_tac_toe/game_state.rb
+++ lib/tic_tac_toe/game_state.rb
@@ -1,10 +1,12 @@
 module TicTacToe
   class GameState
-    attr_accessor :board, :current_player
+    attr_reader :board, :current_player_mark
+    attr_accessor :successors
 
-    def initialize(board, current_player)
-      self.board = board
-      self.current_player = current_player
+    def initialize(board, current_player_mark)
+      @board = board
+      @current_player_mark = current_player_mark
+      @successors = []
     end
 
     def score
@@ -12,12 +14,20 @@ module TicTacToe
         return 0
       end
 
-      if board.winning_mark == current_player.mark
+      if board.winning_mark == current_player_mark
         return 1
       else
         return -1
       end
     end
 
+    def generate_successors
+      next_player_mark = (current_player_mark == "X" ? "O" : "X")
+      board.available_moves.each do |space|
+        next_board = TicTacToe::Board.new(board.grid.dup)
+        next_board.mark_space(space, current_player_mark)
+        @successors << TicTacToe::GameState.new(next_board, next_player_mark)
+      end
+    end
   end
 end
--- spec/tic_tac_toe/game_state_spec.rb
+++ spec/tic_tac_toe/game_state_spec.rb
@@ -9,23 +9,39 @@ describe TicTacToe::GameState do
   describe "#score" do
     it "returns 1 for current player win" do
       board = TicTacToe::Board.new ["X", "O", 3, 4, "X", "O", 7, 8, "X"]
-      current_player = player_x
-      game_state = TicTacToe::GameState.new(board, current_player)
+      current_player_mark = "X"
+      game_state = TicTacToe::GameState.new(board, current_player_mark)
       expect(game_state.score).to eql 1
     end
 
     it "returns 0 if draw" do
       board = TicTacToe::Board.new ["X", "O", "X", "O", "X", "O", "O", "X", "O"]
-      current_player = player_x
-      game_state = TicTacToe::GameState.new(board, current_player)
+      current_player_mark = "X"
+      game_state = TicTacToe::GameState.new(board, current_player_mark)
       expect(game_state.score).to eql 0
     end
 
     it "returns -1 for current player loss" do
       board = TicTacToe::Board.new ["O", "X", 3, 4, "O", "X", 7, 8, "O"]
-      current_player = player_x
-      game_state = TicTacToe::GameState.new(board, current_player)
+      current_player_mark = "X"
+      game_state = TicTacToe::GameState.new(board, current_player_mark)
       expect(game_state.score).to eql -1
+    end
+  end
+
+  describe "#generate_successors" do
+    it "returns the game state's successors" do
+      board = TicTacToe::Board.new [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      current_player_mark = "X"
+      game_state = TicTacToe::GameState.new(board, current_player_mark)
+
+      game_state.generate_successors
+
+      expect(game_state.successors.count).to eql 9
+      game_state.successors.each do |state|
+        expect(state).to be_kind_of(TicTacToe::GameState)
+      end
+      expect(game_state.successors.first.board.grid).to eql ["X", 2, 3, 4, 5, 6, 7, 8, 9]
     end
   end
 end
