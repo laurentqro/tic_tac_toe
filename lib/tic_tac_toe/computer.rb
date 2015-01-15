d57cdadfc5d50c9dfa81436778633bbbd5dee0a9
Laurent Curau <laurentcurau@gmail.com>
2015-01-15 17:50:42 +0000

    Computer#pick_move uses minimax algorithm to get best move

--- lib/tic_tac_toe/computer.rb
+++ lib/tic_tac_toe/computer.rb
@@ -1,19 +1,9 @@
 require 'tic_tac_toe/player'
+require 'tic_tac_toe/game_state'
 
 module TicTacToe
   class Computer < Player
     attr_accessor :mark
-
-    STRATEGIES = {
-      win: lambda { |context| return context.winning_space(context.mark) },
-      block_win: lambda { |context| return context.winning_space(context.opponent_mark) },
-      fork: lambda { |context| return context.space_to_fork(context.mark, context.board) },
-      block_fork: lambda { |context| return context.attacking_moves.first || context.space_to_fork(context.opponent_mark, context.board) },
-      play_center: lambda { |context| return 5 if context.board.available_moves.include? 5 },
-      play_opposite_corner: lambda { |context| return context.available_opposite_corner.first },
-      play_corner: lambda { |context| return context.board.corners.grep(Fixnum).first },
-      play_free_space: lambda { |context| return context.board.available_moves.first }
-    }
 
     def choose_mark
       opponent_mark = ["X", "O"].find { |mark| board.grid.grep(mark).count.odd? }
@@ -22,92 +12,37 @@ module TicTacToe
 
     def pick_move
       mark = @mark || choose_mark
-      best_strategy = STRATEGIES.values.find { |strategy| strategy.call(self) }
-      best_strategy.call(self)
-    end
-
-    # Public helper methods
-
-    def winning_space(mark)
-      winning_spaces(mark, board).first
-    end
-
-    def opponent_mark
-      @mark == "X" ? "O" : "X"
-    end
-
-    def space_to_fork(mark, board)
-      board.available_moves.find do |space|
-        fake_board = create_fake_board
-        fake_board.mark_space(space, mark)
-        winning_spaces(mark, fake_board).count == 2
-      end
-    end
-
-    def attacking_moves
-      spaces_on_triples_with_only_one_mark(mark).reject { |move| results_in_space_to_fork?(move) }
-    end
-
-    def available_opposite_corner
-      board.grid.map.with_index do |space, index|
-        board.opposite_corner(index) if space == opponent_mark && board.opposite_corner(index).is_a?(Fixnum)
-      end
+      minimax( GameState.new(board, mark, mark) )
     end
 
     private
 
-    def winning_spaces(mark, board)
-      winning_triples(mark, board).map do |winning_triple|
-        available_space_on_triple(winning_triple)
+    def minimax(game_state, depth = 0, move_scores = {})
+      return game_state.final_state_score if game_state.board.winner? || game_state.board.draw?
+      next_player_mark = (game_state.current_player_mark == "X" ? "O" : "X")
+
+      game_state.board.available_moves.each do |move|
+        next_board = TicTacToe::Board.new(game_state.board.grid.dup)
+        next_board.mark_space(move, game_state.current_player_mark)
+        next_game_state = TicTacToe::GameState.new(next_board, next_player_mark, game_state.max_player_mark)
+        move_scores[move] = minimax(next_game_state, depth += 1, {})
+      end
+
+      if depth == game_state.board.available_moves.length
+        return move_scores.max_by { |move, score| score }[0]
+      end
+
+      if game_state.current_player_mark == mark
+        return move_scores.max_by { |move, score| score }[1]
+      end
+
+      if game_state.current_player_mark == opponent_mark
+        return move_scores.min_by { |move, score| score }[1]
       end
     end
 
-    def winning_space_on_triple?(mark, triple)
-      two_in_a_row_on_triple?(mark, triple) && available_space_on_triple?(triple)
-    end
-
-    def two_in_a_row_on_triple?(mark, triple)
-      triple.count(mark) == 2
-    end
-
-    def available_space_on_triple(triple)
-      triple.grep(Integer)[0]
-    end
-
-    def available_space_on_triple?(triple)
-      available_space_on_triple(triple) != nil
-    end
-
-    def results_in_space_to_fork?(move)
-      board.available_moves.any? do |space|
-        fake_board = create_fake_board
-        fake_board.mark_space(space, mark)
-        space_to_fork(opponent_mark, fake_board) == nil
-      end
-    end
-
-    def winning_triples(mark, board)
-      board.triples.select do
-        |triple| winning_space_on_triple?(mark, triple)
-      end
-    end
-
-    def create_fake_board
-      TicTacToe::Board.new(board.grid.dup)
-    end
-
-    def triples_with_only_one_mark(mark)
-      board.triples.select do |triple|
-        triple.grep(Fixnum).count == 2 && triple.grep(mark).count == 1
-      end
-    end
-
-    def spaces_on_triples_with_only_one_mark(mark)
-      if triples_with_only_one_mark(mark).any?
-        triples_with_only_one_mark(mark).flatten!.uniq.grep(Integer)
-      else
-        []
-      end
+    def opponent_mark
+      @mark == "X" ? "O" : "X"
     end
   end
 end
--- spec/tic_tac_toe/computer_spec.rb
+++ spec/tic_tac_toe/computer_spec.rb
@@ -25,86 +25,36 @@ describe TicTacToe::Computer do
   end
 
   describe "#pick_move" do
-    context "when it has two in a row" do
-      it "places a third to get three in a row" do
-        board = TicTacToe::Board.new ["X", "O", 3, 4, "X", 6, "O", 8 , 9]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql ["X", "O", 3, 4, "X", 6, "O", 8 , "X"]
-      end
+    it "goes for the win" do
+      board = TicTacToe::Board.new ["O", "X",  3,
+                                     4,  "X", "X",
+                                    "O", "O",  9]
+      computer.board = board
+      expect(computer.pick_move).to eql 4
     end
 
-    context "when the opponent has two in a row" do
-      it "plays the third to block the opponent" do
-        board = TicTacToe::Board.new ["O", "O", 3, 4, "X", 6, 7, "X", 9]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql ["O", "O", "X", 4, "X", 6, 7, "X", 9]
-      end
+    it "blocks opponent's win" do
+      board = TicTacToe::Board.new ["O", "X",  3,
+                                     4,   5 ,  6,
+                                     7,   8 , "O"]
+      computer.board = board
+      expect(computer.pick_move).to eql 5
     end
 
-    context "when it has two non-blocked lines of two" do
-      it "creates a fork (two threats to win)" do
-        board = TicTacToe::Board.new ["X", "O", 3, 4, "X", 6, 7, 8, "O"]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql ["X", "O", 3, "X", "X", 6, 7, 8, "O"]
-      end
+    it "creates a fork (two threats to win)" do
+      board = TicTacToe::Board.new ["X", "O",  3,
+                                     4,  "X",  6,
+                                     7,   8,  "O"]
+      computer.board = board
+      expect(computer.pick_move).to eql 4
     end
 
-    context "when opponent has the opportunity to create a fork" do
-      it "creates two in a row to force the opponent into defending, without creating an opportunity for opponent to fork" do
-        board = TicTacToe::Board.new [ "O", 2, 3, 4, "X", 6, 7, 8, "O"]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql ["O", 2, 3, "X", "X", 6, 7, 8, "O"]
-      end
-
-      it "blocks opponent's fork" do
-        board = TicTacToe::Board.new ["O", 2, 3, 4, "X", "O", 7, 8, 9]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql ["O", 2, "X", 4, "X", "O", 7, 8, 9]
-      end
-    end
-
-    context "when there is no opportunity to win, block, fork, or block a fork" do
-      it "plays center" do
-        board = TicTacToe::Board.new [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql [1, 2, 3, 4, "X", 6, 7, 8, 9]
-      end
-
-      it "plays the opposite corner if the opponent is in a corner" do
-        board = TicTacToe::Board.new ["O", 2, 3, 4, "X", 6, 7, 8, 9]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql ["O", 2, 3, 4, "X", 6, 7, 8, "X"]
-      end
-
-      it "plays the first of the available corners if a corner is available" do
-        board = TicTacToe::Board.new [1, 2, 3, 4, "O", 6, 7, 8, 9]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql ["X", 2, 3, 4, "O", 6, 7, 8, 9]
-      end
-
-      it "plays the first of the four available sides if everything else is taken" do
-        board = TicTacToe::Board.new ["X", "O", "X", 4, "X", "O", "O", "X", "O"]
-        computer.board = board
-        move = computer.pick_move
-        board.mark_space(move, computer.mark)
-        expect(board.grid).to eql ["X", "O", "X", "X", "X", "O", "O", "X", "O"]
-      end
+    it "creates two in a row to force the opponent into defending" do
+      board = TicTacToe::Board.new ["O",  2,   3,
+                                     4,  "X",  6,
+                                     7,   8,  "O"]
+      computer.board = board
+      expect(computer.pick_move).to eql 2
     end
   end
 end
