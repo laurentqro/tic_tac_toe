2e63b9f3ec64e8faa2e8419389e053c5019e5120
Laurent Curau <laurentcurau@gmail.com>
2015-01-15 11:07:09 +0000

    GameState#generate_tree generates game tree for node

--- lib/tic_tac_toe/game_state.rb
+++ lib/tic_tac_toe/game_state.rb
@@ -1,33 +1,29 @@
 module TicTacToe
   class GameState
     attr_reader :board, :current_player_mark
-    attr_accessor :successors
+    attr_accessor :children
 
     def initialize(board, current_player_mark)
       @board = board
       @current_player_mark = current_player_mark
-      @successors = []
+      @children = []
     end
 
-    def score
-      if board.draw?
-        return 0
-      end
-
-      if board.winning_mark == current_player_mark
-        return 1
-      else
-        return -1
-      end
-    end
-
-    def generate_successors
+    def generate_tree
       next_player_mark = (current_player_mark == "X" ? "O" : "X")
-      board.available_moves.each do |space|
+      board.available_moves.each do |move|
         next_board = TicTacToe::Board.new(board.grid.dup)
-        next_board.mark_space(space, current_player_mark)
-        @successors << TicTacToe::GameState.new(next_board, next_player_mark)
+        next_board.mark_space(move, current_player_mark)
+        child_game_state = TicTacToe::GameState.new(next_board, next_player_mark)
+        children << child_game_state
+        child_game_state.generate_tree
       end
+    end
+
+    def final_state_score
+      return 0 if board.draw?
+      return 1 if board.winning_mark == current_player_mark
+      return -1
     end
   end
 end
--- spec/tic_tac_toe/game_state_spec.rb
+++ spec/tic_tac_toe/game_state_spec.rb
@@ -1,47 +1,46 @@
 require 'tic_tac_toe/game_state'
-require 'tic_tac_toe/game'
+require 'tic_tac_toe/board'
+require 'tic_tac_toe/computer'
 
 describe TicTacToe::GameState do
 
   let(:player_x) { TicTacToe::Computer.new(mark: "X") }
   let(:player_o) { TicTacToe::Computer.new(mark: "O") }
 
-  describe "#score" do
+  describe "#final_state_score" do
     it "returns 1 for current player win" do
-      board = TicTacToe::Board.new ["X", "O", 3, 4, "X", "O", 7, 8, "X"]
-      current_player_mark = "X"
-      game_state = TicTacToe::GameState.new(board, current_player_mark)
-      expect(game_state.score).to eql 1
+      board = TicTacToe::Board.new ["X", "O",  3,
+                                     4,  "X", "O",
+                                     7,   8,  "X"]
+      game_state = TicTacToe::GameState.new(board, "X")
+      expect(game_state.final_state_score).to eql 1
     end
 
     it "returns 0 if draw" do
       board = TicTacToe::Board.new ["X", "O", "X", "O", "X", "O", "O", "X", "O"]
-      current_player_mark = "X"
-      game_state = TicTacToe::GameState.new(board, current_player_mark)
-      expect(game_state.score).to eql 0
+      game_state = TicTacToe::GameState.new(board, "X")
+      expect(game_state.final_state_score).to eql 0
     end
 
     it "returns -1 for current player loss" do
       board = TicTacToe::Board.new ["O", "X", 3, 4, "O", "X", 7, 8, "O"]
-      current_player_mark = "X"
-      game_state = TicTacToe::GameState.new(board, current_player_mark)
-      expect(game_state.score).to eql -1
+      game_state = TicTacToe::GameState.new(board, "X")
+      expect(game_state.final_state_score).to eql -1
     end
   end
 
-  describe "#generate_successors" do
-    it "returns the game state's successors" do
+  describe "#generate_tree" do
+    it "generates the game tree for the given game state" do
       board = TicTacToe::Board.new [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      current_player_mark = "X"
-      game_state = TicTacToe::GameState.new(board, current_player_mark)
-
-      game_state.generate_successors
-
-      expect(game_state.successors.count).to eql 9
-      game_state.successors.each do |state|
-        expect(state).to be_kind_of(TicTacToe::GameState)
+      game_state = TicTacToe::GameState.new(board, "X")
+      game_state.generate_tree
+      expect(game_state.children.count).to eql 9
+      game_state.children.each do |game_state_child|
+        expect(game_state_child).to be_kind_of(TicTacToe::GameState)
       end
-      expect(game_state.successors.first.board.grid).to eql ["X", 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(game_state.children.first.board.grid).to eql ["X", 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(game_state.children.last.board.grid).to eql [1, 2, 3, 4, 5, 6, 7, 8, "X"]
+      expect(game_state.children.first.current_player_mark).to eql "O"
     end
   end
 end
