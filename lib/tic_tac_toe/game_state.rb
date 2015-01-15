e5e3570d2d64ffbf2a3a06f3b01e9bc46dbbad27
Laurent Curau <laurentcurau@gmail.com>
2015-01-15 17:48:05 +0000

    Remove #generate_tree and fix #final_state_score

--- lib/tic_tac_toe/game_state.rb
+++ lib/tic_tac_toe/game_state.rb
@@ -1,28 +1,16 @@
 module TicTacToe
   class GameState
-    attr_reader :board, :current_player_mark
-    attr_accessor :children
+    attr_reader :board, :current_player_mark, :max_player_mark
 
-    def initialize(board, current_player_mark)
+    def initialize(board, current_player_mark, max_player_mark)
       @board = board
       @current_player_mark = current_player_mark
-      @children = []
-    end
-
-    def generate_tree
-      next_player_mark = (current_player_mark == "X" ? "O" : "X")
-      board.available_moves.each do |move|
-        next_board = TicTacToe::Board.new(board.grid.dup)
-        next_board.mark_space(move, current_player_mark)
-        child_game_state = TicTacToe::GameState.new(next_board, next_player_mark)
-        children << child_game_state
-        child_game_state.generate_tree
-      end
+      @max_player_mark = max_player_mark
     end
 
     def final_state_score
       return 0 if board.draw?
-      return 1 if board.winning_mark == current_player_mark
+      return 1 if board.winning_mark == max_player_mark
       return -1
     end
   end
--- spec/tic_tac_toe/game_state_spec.rb
+++ spec/tic_tac_toe/game_state_spec.rb
@@ -4,43 +4,25 @@ require 'tic_tac_toe/computer'
 
 describe TicTacToe::GameState do
 
-  let(:player_x) { TicTacToe::Computer.new(mark: "X") }
-  let(:player_o) { TicTacToe::Computer.new(mark: "O") }
-
   describe "#final_state_score" do
-    it "returns 1 for current player win" do
-      board = TicTacToe::Board.new ["X", "O",  3,
-                                     4,  "X", "O",
-                                     7,   8,  "X"]
-      game_state = TicTacToe::GameState.new(board, "X")
-      expect(game_state.final_state_score).to eql 1
-    end
-
-    it "returns 0 if draw" do
-      board = TicTacToe::Board.new ["X", "O", "X", "O", "X", "O", "O", "X", "O"]
-      game_state = TicTacToe::GameState.new(board, "X")
-      expect(game_state.final_state_score).to eql 0
-    end
-
-    it "returns -1 for current player loss" do
-      board = TicTacToe::Board.new ["O", "X", 3, 4, "O", "X", 7, 8, "O"]
-      game_state = TicTacToe::GameState.new(board, "X")
-      expect(game_state.final_state_score).to eql -1
-    end
-  end
-
-  describe "#generate_tree" do
-    it "generates the game tree for the given game state" do
-      board = TicTacToe::Board.new [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      game_state = TicTacToe::GameState.new(board, "X")
-      game_state.generate_tree
-      expect(game_state.children.count).to eql 9
-      game_state.children.each do |game_state_child|
-        expect(game_state_child).to be_kind_of(TicTacToe::GameState)
+    context "when game is in final state" do
+      it "returns 1 if maximizing player win" do
+        board = TicTacToe::Board.new ["X", "O", 3, 4, "X", "O", 7, 8, "X"]
+        game_state = TicTacToe::GameState.new(board, "X", "X")
+        expect(game_state.final_state_score).to eql 1
       end
-      expect(game_state.children.first.board.grid).to eql ["X", 2, 3, 4, 5, 6, 7, 8, 9]
-      expect(game_state.children.last.board.grid).to eql [1, 2, 3, 4, 5, 6, 7, 8, "X"]
-      expect(game_state.children.first.current_player_mark).to eql "O"
+
+      it "returns 0 if draw" do
+        board = TicTacToe::Board.new ["X", "O", "X", "O", "X", "O", "O", "X", "O"]
+        game_state = TicTacToe::GameState.new(board, "X", "X")
+        expect(game_state.final_state_score).to eql 0
+      end
+
+      it "returns -1 if maximizing player loss" do
+        board = TicTacToe::Board.new ["O", "X", 3, 4, "O", "X", 7, 8, "O"]
+        game_state = TicTacToe::GameState.new(board, "X", "X")
+        expect(game_state.final_state_score).to eql -1
+      end
     end
   end
 end
